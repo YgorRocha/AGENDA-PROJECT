542fead02d6648848c956bdec10260d4c82c130b
Ygor Rocha <65311638+euygao@users.noreply.github.com>
2021-05-18 21:17:02 -0300

    AGENDA

---
+++ script.js
@@ -0,0 +1,6 @@
+let btn1 = document.getElementById('btn1')
+let btn2 = document.getElementById('btn2')
+let btn3 = document.getElementById('btn3')
+let btn4 = document.getElementById('btn4')
+let btn5 = document.getElementById('btn5')
+
---
+++ script2.js
@@ -0,0 +1,6 @@
+let enviar = document.getElementById('send')
+
+function enviou (){
+
+    alert('Você está agendado')
+}
